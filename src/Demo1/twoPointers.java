519c7ec79ca0fa977560009d4cf540b63bb933c4
Rational <2760535020@qq.com>
2026-07-20 16:39:43 +0800

    双指针Day2

--- src/Demo1/twoPointers.java
+++ src/Demo1/twoPointers.java
@@ -1,5 +1,9 @@
 package Demo1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class twoPointers {
 /*
         常⻅的双指针有两种形式，⼀种是对撞指针，⼀种是左右指针。
@@ -74,5 +78,165 @@ public class twoPointers {
         }
     }
 
+    /**
+     *  快乐数  https://leetcode.cn/problems/happy-number/description/
+     */
+    class Solution {
+        public boolean isHappy(int n) {
+            int fast = sq(sq(n));
+            int slow = sq(n);
+            while(fast != slow){
+                fast = sq(fast);
+                fast = sq(fast);
+                slow = sq(slow);
+            }
+            if(fast == 1) {return true;}
+            else return false;
+        }
 
+        public int sq(int num){
+            int ret = 0;
+            int x = 0;
+            while(num > 0){
+                x = (num) % 10;
+                ret += x * x;
+                num = num / 10;
+            }
+            return ret;
+        }
+    }
+
+    /**
+     * https://leetcode.cn/problems/container-with-most-water/description/
+     * @param h
+     * @return
+     */
+        public int maxArea(int[] h) {
+            int n = h.length;
+            int ret = 0;
+            int left = 0 , right = n - 1;
+            while(left != right){
+                ret = Math.max(ret , Math.min(h[left] , h[right])*(right - left));
+                if(h[left] < h[right]) left++;
+                else right--;
+            }
+            return ret;
+        }
+
+
+    /**
+     * 有效三角形的个数   https://leetcode.cn/problems/valid-triangle-number/description/
+     * @param nums
+     * @return
+     */
+    public int triangleNumber(int[] nums) {
+        int n = nums.length;
+        if(n == 1 || n == 2) return 0;
+        Arrays.sort(nums);
+        int ret = 0;
+        for(int i = n - 1 ; i >= 2 ;i--){
+            int left = 0 , right = i - 1;
+            while(left != right){
+                if(nums[left] + nums[right] > nums[i]){
+                    ret += (right - left);
+                    right--;
+                }else{
+                    left++;
+                }
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * https://leetcode.cn/problems/he-wei-sde-liang-ge-shu-zi-lcof/description/
+     * @param price
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] price, int target) {
+        int n = price.length;
+        int left = 0 ,right = n - 1;
+
+        int[] ret;
+        while(left != right){
+            int sum = price[left] + price[right];
+            if(sum > target){
+                right--;
+            }else if(sum < target){
+                left++;
+            }else{
+                return new int[]{price[left] , price[right]};
+            }
+        }
+        return new int[0];
+    }
+
+    /**
+     * https://leetcode.cn/problems/3sum/
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = n - 1; i >= 2;) {
+            int left = 0, right = i - 1;
+            int target = -nums[i];
+            if (target > 0) break;  // ← 见下面说明
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum > target) right--;
+                else if (sum < target) left++;
+                else {
+                    ret.add(Arrays.asList(nums[left], nums[right], nums[i]));
+                    left++; right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+            }
+            i--;
+            while (i >= 2 && nums[i] == nums[i + 1]) i--;
+        }
+        return ret;
+    }
+
+    /**
+     * 四数之和 https://leetcode.cn/problems/4sum/description/
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ret = new ArrayList<>();
+        Arrays.sort(nums);  // ← 补上排序
+        int n = nums.length;
+
+        for (int i = n - 1; i >= 3;) {
+            for (int j = i - 1; j >= 2;) {
+                long aim = (long) target - nums[i] - nums[j];  // ← 用 long
+                int left = 0, right = j - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[left] + nums[right];  // ← 用 long
+                    if (sum > aim) right--;
+                    else if (sum < aim) left++;
+                    else {
+                        ret.add(Arrays.asList(nums[left], nums[right], nums[j], nums[i]));
+                        left++; right--;
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
+                    }
+                }
+                j--;
+                while (j >= 2 && nums[j] == nums[j + 1]) j--;
+            }
+            i--;
+            while (i >= 3 && nums[i] == nums[i + 1]) i--;
+        }
+        return ret;
+    }
 }
